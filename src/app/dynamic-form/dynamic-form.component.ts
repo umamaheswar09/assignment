abfadb944409b4ef31de9dcf2bd419d6412710a7
umamaheswar09 <umamaheswarasai.danda@snaddevelopers.com>
2024-08-19 23:59:11 +0530

    updated code

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -13,61 +13,154 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './dynamic-form.component.html',
   styleUrl: './dynamic-form.component.scss'
 })
-export class DynamicFormComponent implements OnInit {
 
-  contactsForm!: FormGroup;
-  storeDataInTable: any[] = [];
+export class DynamicFormComponent {
 
-  constructor(private fb: FormBuilder, private modalService: NgbModal) { }
+  // contactsForm!: FormGroup;
+  // storeDataInTable: any[] = [];
 
-  ngOnInit(): void {
-    this.initiateForm();
-  }
+  // constructor(private fb: FormBuilder, private modalService: NgbModal) { }
 
-  initiateForm() {
-    this.contactsForm = this.fb.group({
-      contacts: this.fb.array([this.createContactForm()])
-    });
-  }
+  // ngOnInit(): void {
+  //   this.initiateForm();
+  // }
 
-  createContactForm(): FormGroup {
-    return this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(15)]],
+  // initiateForm() {
+  //   this.contactsForm = this.fb.group({
+  //     contacts: this.fb.array([this.createContactForm()])
+  //   });
+  // }
+
+  // createContactForm(): FormGroup {
+  //   return this.fb.group({
+  //     name: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(15)]],
+  //     email: ['', [Validators.required, Validators.email]],
+  //     phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+  //   });
+  // }
+
+  // get contacts(): FormArray {
+  //   return this.contactsForm.get('contacts') as FormArray;
+  // }
+
+  // addContact() {
+  //   this.contacts.push(this.createContactForm());
+  // }
+
+  // removeContact(index: number, content: any) {
+  //   // Open the confirmation modal
+  //   this.modalService.open(content).result.then((result) => {
+  //     if (result === 'Yes') {
+  //       this.contacts.removeAt(index);
+  //     }
+  //   }, (reason) => {
+  //     console.log('Dismissed');
+  //   });
+  // }
+
+  // submitForm() {
+  //   if (this.contactsForm.valid) {
+  //     this.storeDataInTable = [...this.contactsForm.value.contacts];
+  //     console.log("Form submitted successfully: ", this.storeDataInTable);
+  //   } else {
+  //     console.log("Form is invalid");
+  //   }
+  // }
+  // contactForm: FormGroup;
+  // contactsList: any[] = [];
+
+  // constructor(private fb: FormBuilder, private modalService: NgbModal) {
+  //   this.contactForm = this.fb.group({
+  //     contacts: this.fb.array([])
+  //   });
+  // }
+
+  // get contacts(): FormArray {
+  //   return this.contactForm.get('contacts') as FormArray;
+  // }
+
+  // addContact(): void {
+  //   const contactFormGroup = this.fb.group({
+  //     name: ['', Validators.required],
+  //     email: ['', [Validators.required, Validators.email]],
+  //     phone: ['', Validators.required]
+  //   });
+  //   this.contacts.push(contactFormGroup);
+  // }
+
+  // updateContact(index: number): void {
+  //   const contact = this.contacts.at(index);
+  //   if (contact.valid) {
+  //     console.log(`Contact ${index + 1} updated:`, contact.value);
+  //   } else {
+  //     contact.markAllAsTouched();
+  //   }
+  // }
+
+  // deleteContact(index: number, content: any): void {
+  //   this.modalService.open(content).result.then((result) => {
+  //     if (result === 'yes') {
+  //       this.contacts.removeAt(index);
+  //       this.contactsList.splice(index, 1);
+  //     }
+  //   }, () => {});
+  // }
+
+  // onSubmit(): void {
+  //   if (this.contactForm.valid) {
+  //     this.contactsList.push(this.contactForm.value); 
+  //     console.log('All Contacts:', this.contactForm.value.contacts);
+  //   } else {
+  //     this.contactForm.markAllAsTouched();
+  //   }
+  // }
+
+  contactForm: FormGroup;
+  showForm: boolean = false;
+  contactsList: any[] = []; // Store contacts to be displayed in the table
+  editIndex: number | null = null; // Track the index of the contact being edited
+
+  constructor(private fb: FormBuilder, private modalService: NgbModal) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      phone: ['', Validators.required]
     });
   }
 
-  get contacts(): FormArray {
-    return this.contactsForm.get('contacts') as FormArray;
+  addContact(): void {
+    this.showForm = true;
+    this.contactForm.reset();
+    this.editIndex = null; // Reset edit mode
   }
 
-  addContact() {
-    this.contacts.push(this.createContactForm());
-  }
-
-  removeContact(index: number, content: any) {
-    // Open the confirmation modal
-    this.modalService.open(content).result.then((result) => {
-      if (result === 'Yes') {
-        this.contacts.removeAt(index);
+  submitContact(): void {
+    if (this.contactForm.valid) {
+      if (this.editIndex !== null) {
+        // If in edit mode, update the existing contact
+        this.contactsList[this.editIndex] = this.contactForm.value;
+      } else {
+        // If not in edit mode, add a new contact
+        this.contactsList.push(this.contactForm.value);
       }
-    }, (reason) => {
-      console.log('Dismissed');
-    });
-  }
-
-  submitForm() {
-    if (this.contactsForm.valid) {
-      this.storeDataInTable = [...this.contactsForm.value.contacts];
-      console.log("Form submitted successfully: ", this.storeDataInTable);
+      this.showForm = false;
     } else {
-      console.log("Form is invalid");
+      this.contactForm.markAllAsTouched();
     }
   }
 
+  updatePhone(index: number): void {
+    const contact = this.contactsList[index];
+    this.contactForm.patchValue(contact); // Load existing contact details into the form
+    this.showForm = true;
+    this.editIndex = index; // Set edit mode
+  }
 
-
-
-
+  deleteContact(index: number, content: any): void {
+    this.modalService.open(content).result.then((result) => {
+      if (result === 'yes') {
+        this.contactsList.splice(index, 1);
+      }
+    }, () => {});
+  }
 }
